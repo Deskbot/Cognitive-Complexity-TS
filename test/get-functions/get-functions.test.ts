2d264676a4edc07c94d6e2278a5c84461dfd4940
Deskbot <Deskbot@users.noreply.github.com>
2020-02-21 12:57:05 +0000

    Add Jasmine

--- test/get-functions/get-functions.test.ts
+++ test/get-functions/get-functions.test.ts
@@ -1,3 +1,22 @@
+import jasmine from "jasmine";
+import path from "path";
+import ts from "typescript";
+
 import { getFunctions } from "../../src/get-functions";
 
-getFunctions
+describe("getting functions from TypeScript source", () => {
+    it("should find no functions in an empty file", () => {
+        const file = ts.createSourceFile(
+            path.basename(__dirname + "./cases/empty.ts"),
+            "./cases/empty.ts",
+            ts.ScriptTarget.ES2017,
+            true,
+        );
+
+        expect(getFunctions(file).length).toBe(0);
+    });
+
+    it("fail", () => {
+        fail();
+    });
+});
