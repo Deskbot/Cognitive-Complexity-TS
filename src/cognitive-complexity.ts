c9994877cf018ad3c068935daf87390a612c494d
Thomas Richards <Deskbot@users.noreply.github.com>
2020-04-13 19:35:30 +0100

    Class-ify

--- src/cognitive-complexity.ts
+++ src/cognitive-complexity.ts
@@ -19,7 +19,7 @@ function calcElemCost(node: ts.Node, depth = 0): OutputElem {
     const { line, character: column } = node.getSourceFile()
         .getLineAndCharacterOfPosition(node.getStart());
 
-    const nodeCost = calcNodeCost(node, depth);
+    const nodeCost = new NodeCost().calculate(node, depth);
     score += nodeCost.score;
     inner.push(...nodeCost.inner);
 
@@ -48,85 +48,87 @@ export function calcFileCost(file: ts.Node): OutputFileElem {
     }
 }
 
-function calcNodeCost(node: ts.Node, depth: number): ScoreAndInner {
-    let score = 0;
-    let inner = [] as OutputElem[];
-    let result: ScoreAndInner;
+class NodeCost {
+    calculate(node: ts.Node, depth: number): ScoreAndInner {
+        let score = 0;
+        let inner = [] as OutputElem[];
+        let result: ScoreAndInner;
 
-    let continueFrom = node.getChildren();
+        let continueFrom = node.getChildren();
 
-    // certain langauge features carry and inherent cost
-    if (ts.isCatchClause(node)
-        || ts.isConditionalExpression(node)
-        || ts.isForInStatement(node)
-        || ts.isForOfStatement(node)
-        || ts.isForStatement(node)
-        || ts.isIfStatement(node)
-        || ts.isSwitchStatement(node)
-        || ts.isWhileStatement(node)
-        || isBreakOrContinueToLabel(node)
-    ) {
-        score += 1;
-    }
+        // certain langauge features carry and inherent cost
+        if (ts.isCatchClause(node)
+            || ts.isConditionalExpression(node)
+            || ts.isForInStatement(node)
+            || ts.isForOfStatement(node)
+            || ts.isForStatement(node)
+            || ts.isIfStatement(node)
+            || ts.isSwitchStatement(node)
+            || ts.isWhileStatement(node)
+            || isBreakOrContinueToLabel(node)
+        ) {
+            score += 1;
+        }
 
-    // increment for nesting level
-    if (depth > 0 && (
-        ts.isConditionalExpression(node)
-        || ts.isForInStatement(node)
-        || ts.isForOfStatement(node)
-        || ts.isForStatement(node)
-        || ts.isIfStatement(node)
-        || ts.isSwitchStatement(node)
-        || ts.isWhileStatement(node)
-    )) {
-        score += depth;
-    }
+        // increment for nesting level
+        if (depth > 0 && (
+            ts.isConditionalExpression(node)
+            || ts.isForInStatement(node)
+            || ts.isForOfStatement(node)
+            || ts.isForStatement(node)
+            || ts.isIfStatement(node)
+            || ts.isSwitchStatement(node)
+            || ts.isWhileStatement(node)
+        )) {
+            score += depth;
+        }
 
-    // certain structures increment depth for their child nodes
-    if (ts.isCatchClause(node)
-        || ts.isConditionalExpression(node)
-        || ts.isDoStatement(node)
-        || ts.isForInStatement(node)
-        || ts.isForOfStatement(node)
-        || ts.isForStatement(node)
-        || ts.isIfStatement(node)
-        || ts.isSwitchStatement(node)
-        || ts.isWhileStatement(node)
-        || (
-            depth !== 0
-            && (
-                ts.isArrowFunction(node)
-                || ts.isFunctionDeclaration(node)
-                || ts.isFunctionExpression(node)
-                || ts.isMethodDeclaration(node)
+        // certain structures increment depth for their child nodes
+        if (ts.isCatchClause(node)
+            || ts.isConditionalExpression(node)
+            || ts.isDoStatement(node)
+            || ts.isForInStatement(node)
+            || ts.isForOfStatement(node)
+            || ts.isForStatement(node)
+            || ts.isIfStatement(node)
+            || ts.isSwitchStatement(node)
+            || ts.isWhileStatement(node)
+            || (
+                depth !== 0
+                && (
+                    ts.isArrowFunction(node)
+                    || ts.isFunctionDeclaration(node)
+                    || ts.isFunctionExpression(node)
+                    || ts.isMethodDeclaration(node)
+                )
             )
-        )
-    ) {
-        // TODO some of the children will have the same depth, some will be depth + 1
-        // the condition of an if/do has the same depth
-        // the block/statement has depth + 1
-        depth += 1;
+        ) {
+            // TODO some of the children will have the same depth, some will be depth + 1
+            // the condition of an if/do has the same depth
+            // the block/statement has depth + 1
+            depth += 1;
+        }
+
+        // TODO write isSequenceOfBinaryOperators to check whether to do an inherent increment
+        // BinaryExpressions have 1 child that is the operator
+        // BinaryExpressions have their last child as a sub expression
+        // can just consume the entire sequence of the same operator
+        // then continue traversing from the next different operator in the sequence,
+        // which presumably will be given another inherent increment by the next call to calcNodeCost
+        // should redundant brackets be ignored? or do they end a sequence?
+        // probably the latter, which would also be easier
+
+        for (const child of continueFrom) {
+            result = new NodeCost().calculate(child, depth);
+            score += result.score;
+            inner.push(...result.inner);
+        }
+
+        return {
+            score,
+            inner,
+        };
     }
-
-    // TODO write isSequenceOfBinaryOperators to check whether to do an inherent increment
-    // BinaryExpressions have 1 child that is the operator
-    // BinaryExpressions have their last child as a sub expression
-    // can just consume the entire sequence of the same operator
-    // then continue traversing from the next different operator in the sequence,
-    // which presumably will be given another inherent increment by the next call to calcNodeCost
-    // should redundant brackets be ignored? or do they end a sequence?
-    // probably the latter, which would also be easier
-
-    for (const child of continueFrom) {
-        result = calcNodeCost(child, depth);
-        score += result.score;
-        inner.push(...result.inner);
-    }
-
-    return {
-        score,
-        inner,
-    };
 }
 
 function isBreakOrContinueToLabel(node: ts.Node): boolean {
