64288d37fce3be0c65048db0ac567f8b76c47a96
Thomas Richards <Deskbot@users.noreply.github.com>
2020-05-02 10:38:03 +0100

    Add test for bitwise operators

---
+++ test/cases/21-bitwise-operators.ts
@@ -0,0 +1,19 @@
+function allAnd() {
+    1 & 1 & 1;
+}
+
+function allOr() {
+    1 | 1 | 1;
+}
+
+function twoSequences() {
+    1 & 1 & 1 | 1 | 1;
+}
+
+function threeSequences() {
+    1 & 1 | 1 | 1 & 1;
+}
+
+function noOp() {
+    1;
+}
