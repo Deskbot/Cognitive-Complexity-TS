c47aca5da863c3b109b813ccb851ed3e400e98b9
Thomas Richards <Deskbot@users.noreply.github.com>
2020-05-04 20:39:52 +0100

    Add tests for getters and setters

---
+++ test/cases/27-accessor-methods.ts
@@ -0,0 +1,13 @@
+class Getter {
+    // depth 0
+    get f(): string {
+        // depth 0
+        return this.f; // +1 for recursion
+    }
+}
+
+class Setter {
+    set f(value) {
+        this.f = () => {};
+    }
+}
