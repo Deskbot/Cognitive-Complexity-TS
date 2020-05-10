69035c9a745218537cf092205e36ec1be32f93df
Thomas Richards <Deskbot@users.noreply.github.com>
2020-05-10 15:33:33 +0100

    Simplify script

--- bin/cognitive-complexity-ts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# one liner off Stack Overflow
-SH_LOCATION="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd )"
-
-node -r source-map-support/register "${SH_LOCATION}/../build/src/main" "$@"
