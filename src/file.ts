f2aea52d828a3cd6bbcdbe858d72284e2ea7b002
Thomas Richards <Deskbot@users.noreply.github.com>
2020-07-26 20:12:10 +0100

    Split file

---
+++ src/file.ts
@@ -0,0 +1,56 @@
+/**
+ * Functions to apply Cognitive Complexity to files and folders.
+ */
+
+ import { promises as fsP } from "fs";
+import * as path from "path";
+import * as ts from "typescript";
+import { FileOutput, FolderOutput } from "./types";
+import { fileCost } from "./cognitive-complexity";
+
+/**
+ * @param entry A file system entry ok unknown type.
+ */
+export async function getEntryOutput(entryPath: string): Promise<FileOutput | FolderOutput> {
+    const entry = await fsP.stat(entryPath);
+
+    if (entry.isDirectory()) {
+        const allSubFiles = await fsP.readdir(entryPath, { withFileTypes: true });
+        const subFiles = allSubFiles
+            .filter(filePath => filePath.name.match(/.*\.[tj]sx?$/) !== null);
+
+        // promises are used instead of await so that spawned promises don't start after the previous ends
+        const folderOutput: FolderOutput = {};
+        const folderOutputPromises: Promise<void>[] = []; // all promises used to build folderOutput
+
+        for (const file of subFiles) {
+            const innerEntryPath = path.join(entryPath, file.name);
+
+            // once we have the entry data, then add it to the output
+            const entryAddedToFolderOutput = getEntryOutput(innerEntryPath)
+                .then(folderEntry => {
+                    folderOutput[file.name] = folderEntry;
+                });
+            folderOutputPromises.push(entryAddedToFolderOutput);
+        }
+
+        await Promise.all(folderOutputPromises);
+
+        return folderOutput;
+    } else {
+        return getFileOutput(entryPath);
+    }
+}
+
+async function getFileOutput(filePath: string): Promise<FileOutput> {
+    const fileContent = (await fsP.readFile(filePath)).toString();
+
+    const parsedFile = ts.createSourceFile(
+        path.basename(filePath),
+        fileContent,
+        ts.ScriptTarget.Latest,
+        true,
+    );
+
+    return fileCost(parsedFile);
+}
