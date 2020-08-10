7e7af72aaa79ec2104f80c8387a6307d15f6a107
Thomas Richards <Deskbot@users.noreply.github.com>
2020-08-10 23:45:49 +0100

    Presumably improve Box efficiency

--- ui/ts/component/Box.ts
+++ ui/ts/component/Box.ts
@@ -1,12 +1,21 @@
-import { element, addStyleSheet, StatefulNode, emptyChildNodes } from "../framework";
+import { element, addStyleSheet, StatefulNode } from "../framework";
 
 addStyleSheet("/css/component/Box");
 
 export class Box implements StatefulNode {
     readonly dom = element("div", { className: "box" });
 
-    rerender(childNodes: (Node | string)[]) {
-        emptyChildNodes(this.dom);
-        this.dom.append(...childNodes);
+    rerender(newChildNodes: (Node | string)[]) {
+        this.dom.prepend(...newChildNodes);
+
+        // only the new child nodes should remain
+        // everything from [0..newChildNodes.length-1] is valid
+        // everything after should be removed
+        for (let indexToRemove = newChildNodes.length;
+            this.dom.childNodes.length > newChildNodes.length;
+            indexToRemove++
+        ) {
+            this.dom.removeChild(this.dom.childNodes[indexToRemove]);
+        }
     }
 }
