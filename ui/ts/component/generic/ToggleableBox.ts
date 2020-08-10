5407d8516b5c5a37343d10ea9508558d6b57cdf8
Thomas Richards <Deskbot@users.noreply.github.com>
2020-08-10 23:07:15 +0100

    Only rerender internally

--- ui/ts/component/generic/ToggleableBox.ts
+++ ui/ts/component/generic/ToggleableBox.ts
@@ -1,43 +1,40 @@
-import { StatefulNode } from "../../framework";
 import { Box } from "../Box";
 import { ToggleButton } from "./ToggleButton";
 
 export function ToggleableBox(visibleContent: Node[], toggleableContent: Node[]): Node {
-    const poop = new Poop();
-    poop.rerender(visibleContent, toggleableContent);
-    return poop.dom;
+    return new Poop(visibleContent, toggleableContent).dom;
 }
 
-class Poop implements StatefulNode {
+class Poop {
     private toggleButton = ToggleButton(false, this.onNewIsOpen.bind(this));
     private box = new Box();
     private showToggleable = false;
 
     readonly dom = this.box.dom;
 
-    private lastCall: [visibleContent: Node[], toggleableContent: Node[]] | undefined;
+    constructor(
+        private visibleContent: Node[],
+        private toggleableContent: Node[]
+    ) {
+        this.rerender();
+    }
 
     private onNewIsOpen(newIsOpen: boolean) {
         this.showToggleable = newIsOpen;
-
-        if (this.lastCall) {
-            this.rerender(...this.lastCall);
-        }
+        this.rerender();
     }
 
-    rerender(visibleContent: Node[], toggleableContent: Node[]) {
-        this.lastCall = [visibleContent, toggleableContent];
-
+    private rerender() {
         const boxContents = [] as Node[];
 
-        if (toggleableContent.length > 0) {
+        if (this.toggleableContent.length > 0) {
             boxContents.push(this.toggleButton);
         }
 
-        boxContents.push(...visibleContent);
+        boxContents.push(...this.visibleContent);
 
         if (this.showToggleable) {
-            boxContents.push(...toggleableContent);
+            boxContents.push(...this.toggleableContent);
         }
 
         this.box.rerender(boxContents);
