005c9c98bfbb11db5fe4929d4fd656eb1d6d1674
Thomas Richards <Deskbot@users.noreply.github.com>
2020-08-13 22:23:00 +0100

    Add copy button for files

--- ui/ts/component/FileComplexity.ts
+++ ui/ts/component/FileComplexity.ts
@@ -1,11 +1,15 @@
 import { FileOutput } from "../../../shared/types";
 import { element } from "../framework";
 import { ContainerComplexity } from "./ContainerComplexity";
+import { CopyButton } from "./generic/CopyableText";
 import { ToggleableBox } from "./generic/ToggleableBox";
 
 export function FileComplexity(filePath: string, complexity: FileOutput, startOpen: boolean): Node {
     return ToggleableBox([
-        element("p", {}, [filePath]),
+        element("p", {}, [
+            filePath,
+            CopyButton(filePath),
+        ]),
         element("p", {}, ["score: " + complexity.score]),
     ],
         complexity.inner.map(ContainerComplexity),
