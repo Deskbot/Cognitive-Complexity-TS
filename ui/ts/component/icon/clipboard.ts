6c29080b3f0d7f32c97fdccc017fb84b52a4e893
Thomas Richards <Deskbot@users.noreply.github.com>
2020-08-22 14:22:03 +0100

    Improve copy button style

--- ui/ts/component/icon/clipboard.ts
+++ ui/ts/component/icon/clipboard.ts
@@ -3,6 +3,6 @@ import { addStyleSheet } from "../../framework";
 addStyleSheet("/css/component/icon/clipboard");
 
 export function ClipboardSvg() {
-    const template = document.getElementById("clipboard-icon") as HTMLTemplateElement;
+    const template = document.getElementById("clipboard-icon-template") as HTMLTemplateElement;
     return template.content.cloneNode(true);
 }
