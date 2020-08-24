97343d3d42e6043c5163245501910b48b4ffd9ba
Thomas Richards <Deskbot@users.noreply.github.com>
2020-08-24 22:53:00 +0100

    Style expand/collapse all buttons

--- TODO.md
+++ TODO.md
@@ -1,6 +1,7 @@
 # Necessary Features
 
 * Add a html interface
+    * expand all doesn't expand all
     * make parent boxes sticky
     * file paths that are too long are truncated at the left
     * sort by complexity
--- ui/ts/component/global-controls.ts
+++ ui/ts/component/global-controls.ts
@@ -1,14 +1,24 @@
-import { element } from "../framework";
+import { addStyleSheet, element } from "../framework";
 import { collapseAllToggleButtons, expandAllToggleButtons } from "./generic/ToggleButton";
 
+addStyleSheet("/css/component/global-controls");
+
 export function ExpandAll(): Node {
-    const button = element("button", {}, "Expand All");
+    const button = element(
+        "button",
+        { className: "global-control-button" },
+        "Expand All"
+    );
     button.addEventListener("click", expandAllToggleButtons);
     return button;
 }
 
 export function CollapseAll(): Node {
-    const button = element("button", {}, "Collapse All");
+    const button = element(
+        "button",
+        { className: "global-control-button" },
+        "Collapse All"
+    );
     button.addEventListener("click", collapseAllToggleButtons);
     return button;
 }
