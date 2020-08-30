7e7dfb7fbe535c06674731bb445fad1ef0423c93
Thomas Richards <Deskbot@users.noreply.github.com>
2020-08-30 17:56:08 +0100

    Add Score component

--- ui/ts/component/FileComplexity.ts
+++ ui/ts/component/FileComplexity.ts
@@ -1,13 +1,13 @@
 import { FileOutput } from "../../../shared/types";
-import { element } from "../framework";
 import { ContainerComplexity } from "./ContainerComplexity";
 import { StickyTitle } from "./StickyTitle";
 import { ToggleableBox } from "./generic/ToggleableBox";
+import { Score } from "./Score";
 
 export function FileComplexity(filePath: string, complexity: FileOutput, startOpen: boolean): ToggleableBox {
     return new ToggleableBox([
         StickyTitle(filePath),
-        element("p", {}, "Score: " + complexity.score),
+        Score(complexity.score),
     ],
         () => complexity.inner.map(complexity => ContainerComplexity(complexity, filePath)),
         startOpen,
