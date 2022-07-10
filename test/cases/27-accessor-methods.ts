0fdf4bb3a64431767a749e8d5c435d7fe8c3f7c0
Thomas Richards <Deskbot@users.noreply.github.com>
2022-07-10 12:03:42 +0100

    feat: add failing test for using computed expression with property access expression

--- test/cases/27-accessor-methods.ts
+++ test/cases/27-accessor-methods.ts
@@ -1,13 +1,25 @@
+var property;
+
 class Getter {
     // depth 0
     get f(): string {
         // depth 0
         return this.f; // +1 for recursion
     }
+
+    // depth 0
+    get [property](): string {
+        // depth 0
+        return this[property]; // +1 for recursion
+    }
 }
 
 class Setter {
     set f(value) {
         this.f = 1;
+    }
+
+    set [property](value) {
+        this[property] = 1;
     }
 }
