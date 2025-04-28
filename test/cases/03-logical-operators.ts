7af04805405b8bbb98309a0bba935a45d0cb6277
Thomas Richards <Deskbot@users.noreply.github.com>
2025-04-28 13:42:25 +0100

    add test cases for logical operators and brackets

--- test/cases/03-logical-operators.ts
+++ test/cases/03-logical-operators.ts
@@ -26,10 +26,42 @@ function parenthesesDoesNotBreakSequence() {
     true && (true && true)
 }
 
+function notBreaksSequence() {
+    true && !(true && true)
+}
+
 function parenthesesAroundASequenceBreak() {
     true && (true || true)
 }
 
-function parenthesesCanBreakSequence() {
+function parenthesesBreakSequence() { // && is child of || due to precedence
     true && (true && true || true)
+}
+
+function sequenceInParentheses1() {
+    true && (true || true && true)
+}
+
+function sequenceInParentheses2() {
+    true || (true && true || true)
+}
+
+function sequenceInParentheses3() {
+    true || (true || true && true)
+}
+
+function sequenceInParentheses4() {
+    true && (true || true) && true
+}
+
+function parenthesesBreakingOneSequenceButNotAnother() {
+    true && (true && true) || true
+}
+
+function parenthesesBreakingTwoSequences() {
+    true || (true || true) && true
+}
+
+function otherOperators() {
+    true == true != true > true < true >= true <= true
 }
