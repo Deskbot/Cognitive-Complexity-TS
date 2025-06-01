e4b2cc36a5b26c9a7946f39d33fdbeb9efb36478
Thomas Richards <Deskbot@users.noreply.github.com>
2025-06-01 14:08:26 +0100

    move traversal context types to be private where used

--- src/cognitive-complexity/cognitive-complexity.ts
+++ src/cognitive-complexity/cognitive-complexity.ts
@@ -1,5 +1,5 @@
 import * as ts from "typescript"
-import { FileOutput, ContainerOutput, ScoreAndInner, TraversalContext, MutableTraversalContext } from "../../shared/types";
+import { FileOutput, ContainerOutput, ScoreAndInner } from "../../shared/types";
 import {
     chooseContainerName,
     getNameIfCalledNode,
@@ -17,6 +17,67 @@ import {
     isBinaryTypeOperator,
 } from "./node-inspection";
 import { Scope } from "./Scope";
+
+interface TraversalContext {
+    /**
+     * Whether the node is at the top level of a file
+     */
+    readonly topLevel: boolean;
+    /**
+     * The depth the node is at
+     */
+    readonly depth: number;
+    /**
+     * Information about scope where the node exists in the code
+     */
+    readonly scope: Scope;
+    /**
+     * If the node is part of a variable being introduced, what is the name of that variable.
+     */
+    readonly variableBeingDefined: string | undefined;
+}
+
+interface MutableTraversalContext {
+    /**
+     * In a sequence of binary operators, which operator are we continuing on from.
+     * During in order traversal, this value will be set by one child node and read by an adjacent child.
+     *
+     * A binary operator node has 2 children, one for each operand.
+     * We do in order traversal, keeping track of the binary operator node (not the operator token).
+     *
+     * A && B && C || D
+     *
+     *              ||
+     *         &&        D
+     *      &&    C
+     *     A  B
+     */
+    precedingOperator: ts.BinaryOperatorToken | undefined;
+
+    /**
+     * In a sequence of type operators, which operator are we continuing on from.
+     * During in order traversal, this value will be set by one child node and read by an adjacent child.
+     *
+     * A type operator node can have many children all joined with the same operator.
+     * We do in order traversal, keeping track of the type operator token (not the type node).
+     *
+     * A & B & C | D
+     *
+     *               |
+     *            &     D
+     *          A B C
+     *
+     * We can't track the operator node itself
+     * because there is always a SyntaxList node between the operator node and the operands.
+     * We can't easily change the latest operator based on the UnionType node, after evaluating the first operand.
+     * UnionType
+     *   SyntaxList
+     *     LiteralType
+     *     BarToken
+     *     LiteralType
+     */
+    precedingTypeOperator: ts.SyntaxKind.AmpersandToken | ts.SyntaxKind.BarToken | undefined;
+}
 
 export function fileCost(file: ts.SourceFile): FileOutput {
     const initialContext: TraversalContext = {
