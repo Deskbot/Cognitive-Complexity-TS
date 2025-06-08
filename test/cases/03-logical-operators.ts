420b48cdd96fc8412b3dc780b21c170c04193d00
Thomas Richards <Deskbot@users.noreply.github.com>
2025-06-08 15:30:00 +0100

    move test

--- test/cases/03-logical-operators.ts
+++ test/cases/03-logical-operators.ts
@@ -62,18 +62,3 @@ function allOtherBreaksInSequenceOfOperators(param = true && true) {
         }
     }
 }
-
-function breaksInSequencesOfTypeOperators<T extends true | true = true | true>(param: true | true) // 3
-    : T | T // 1
-{
-    type U<T> = T | (T | T) // 2
-    let a: NonNullable<1 | 1> | number | NonNullable<1 | 1>  // 2
-        = 1 | 1 as 1 | 1 as 1 | 1; // 2
-    <V extends 1 | number = 1 | 1>(b: 1 | number = 1 | 1): 1 | undefined => { 1 | 1; return undefined } // 4
-    function f<X extends T | T = T | T>(arg: T | T): T | T { return true as T | T } // 5
-    class C<Y extends T | T> { // 1
-        y: T | T // 1
-        z<A extends T | T = T | T>(a: T | T): T | undefined { return undefined } // 4
-    }
-    return true as T
-}
